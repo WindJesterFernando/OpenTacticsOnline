d6f9722d9964ab6db7ee8669aad049dc0d343c64
Fernando Restituto <linksgreenhat@gmail.com>
2024-01-15 13:10:27 -0500

    QueueTest

--- OpenTacticsOnline/Assets/QueueTest.cs
+++ OpenTacticsOnline/Assets/QueueTest.cs
@@ -7,7 +7,15 @@ public class QueueTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("1");
+        //wait 2 seconds
+        Debug.Log("2");
+        //wait 3 seconds
+        Debug.Log("3");
+        //wait 4 seconds
+        Debug.Log("4");
+        //wait 5 seconds
+        Debug.Log("5");
     }
 
     // Update is called once per frame
