61baec7e8804ad76cbeb3a63ed135902d7d5bc27
Fernando Restituto <linksgreenhat@gmail.com>
2024-01-29 14:04:24 -0500

    mid A* clean up

--- OpenTacticsOnline/Assets/Scripts/BattleGridModelData.cs
+++ OpenTacticsOnline/Assets/Scripts/BattleGridModelData.cs
@@ -1,20 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data.Common;
+//using System.ComponentModel;
+//using System.Data.Common;
 using System.Linq;
 using UnityEngine;
 
-public static class BattleGridModelData
+public static partial class BattleGridModelData
 {
     const int gridSizeX = 20, gridSizeY = 10;
     static BattleGridTile[,] battleGridTiles;
 
-    const int MoveCost = 1;
-
-    const  float delayBetweenMoves = 0;
-    
     public static void Init()
     {
         #region Setup & isWalkable Default
@@ -96,178 +92,6 @@ public static class BattleGridModelData
     {
         battleGridTiles[coord.x, coord.y].id = newID;
         GridVisuals.UpdateGridTileSprite(coord, newID);
-    }
-
-    public static List<Vector2Int> DoTheAStarThingMyGuy(Vector2Int start, Vector2Int end)
-    {
-        SetAllTilesToDefault();
-
-        ChangeTileID(start, 104);
-        ChangeTileID(end, 107);
-
-        LinkedList<Vector2Int> visitedTiles = new LinkedList<Vector2Int>();
-        LinkedList<Vector2Int> neighbourTiles = new LinkedList<Vector2Int>();
-
-        int[,] travelDistancesFromStart = new int[gridSizeX, gridSizeY];
-
-        for (int x = 0; x < gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                travelDistancesFromStart[x,y] = -1;
-            }
-        }
-
-
-        visitedTiles.AddLast(start);
-        travelDistancesFromStart[start.x, start.y] = 0;
-
-        foreach (Vector2Int n in GetWalkableNeighbours(start))
-        {
-            neighbourTiles.AddLast(n);
-            travelDistancesFromStart[n.x, n.y] = MoveCost;
-        }
-
-        bool isFound = false;
-
-        while (neighbourTiles.Count > 0)
-        {
-            Vector2Int tileToEvaluate = neighbourTiles.First.Value;
-            int currentlyFoundMinDistance = GetDistance(tileToEvaluate, end) + travelDistancesFromStart[tileToEvaluate.x, tileToEvaluate.y];
-
-            foreach (Vector2Int n in neighbourTiles)
-            {
-                int neighbourDistance = GetDistance(n, end) + travelDistancesFromStart[n.x, n.y];
-
-                if (neighbourDistance < currentlyFoundMinDistance)
-                {
-                    currentlyFoundMinDistance = neighbourDistance;
-                    tileToEvaluate = n;
-                }
-            }
-
-            QueueTest.instance.EnqueueAction(new ActionChangeTileContainer(tileToEvaluate, 101));
-            QueueTest.instance.EnqueueAction(new ActionWaitContainer(delayBetweenMoves));
-
-            neighbourTiles.Remove(tileToEvaluate);
-            visitedTiles.AddLast(tileToEvaluate);
-
-            foreach (Vector2Int neighbour in GetWalkableNeighbours(tileToEvaluate))
-            {
-                if (!neighbourTiles.Contains(neighbour) && !visitedTiles.Contains(neighbour))
-                {
-                    neighbourTiles.AddLast(neighbour);
-                    QueueTest.instance.EnqueueAction(new ActionChangeTileContainer(neighbour, 106));
-                    QueueTest.instance.EnqueueAction(new ActionWaitContainer(delayBetweenMoves));
-                }
-
-                int prevTileMoveCost =  travelDistancesFromStart[tileToEvaluate.x, tileToEvaluate.y];
-                int neighbourTileMoveCost = prevTileMoveCost + MoveCost;
-
-                
-                if(travelDistancesFromStart[neighbour.x, neighbour.y] == -1)
-                    travelDistancesFromStart[neighbour.x, neighbour.y] = neighbourTileMoveCost;
-                else if(travelDistancesFromStart[neighbour.x, neighbour.y] > neighbourTileMoveCost)
-                    travelDistancesFromStart[neighbour.x, neighbour.y] = neighbourTileMoveCost;
-
-                QueueTest.instance.EnqueueAction(new ActionDebugLogContainer(neighbour + " : " + travelDistancesFromStart[neighbour.x, neighbour.y]));
-
-                if (end == neighbour)
-                {
-                    Debug.Log("Found!");
-                    isFound = true;
-                    break;
-                }
-            }
-
-            if (isFound)
-                break;
-        }
-
-
-        if (!isFound)
-            return null;
-        
-    
-        Vector2Int currentTile = end;
-        LinkedList<Vector2Int> path = new LinkedList<Vector2Int>();
-        while (currentTile != start)
-        {
-            path.AddFirst(currentTile);
-            int smallestDistanceToStart = Int32.MaxValue;
-            Vector2Int nextCoord = new Vector2Int();
-            foreach (Vector2Int neighbour in GetWalkableNeighbours(currentTile))
-            {
-                if (travelDistancesFromStart[neighbour.x, neighbour.y] == -1) continue;
-                
-                if (smallestDistanceToStart > travelDistancesFromStart[neighbour.x, neighbour.y])
-                {
-                    smallestDistanceToStart = travelDistancesFromStart[neighbour.x, neighbour.y];
-                    nextCoord = neighbour;
-                }
-            }
-
-            currentTile = nextCoord;
-        }
-
-        foreach (Vector2Int tile in path)
-        {
-            QueueTest.instance.EnqueueAction(new ActionChangeTileContainer(tile, 17));
-            QueueTest.instance.EnqueueAction(new ActionWaitContainer(delayBetweenMoves));
-        }
-
-        return path.ToList();
-    }
-
-    
-    public static int GetDistance(Vector2Int start, Vector2Int end)
-    {
-        Vector2Int dif = end - start;
-        return Math.Abs(dif.x) + Math.Abs(dif.y);
-    }
-
-    public static LinkedList<Vector2Int> GetWalkableNeighbours(Vector2Int coord)
-    {
-        LinkedList<Vector2Int> walkableNeighbours = new LinkedList<Vector2Int>();
-
-        Vector2Int leftCoord = coord + Vector2Int.left;
-        Vector2Int rightCoord = coord + Vector2Int.right;
-        Vector2Int topCoord = coord + Vector2Int.up;
-        Vector2Int bottomCoord = coord + Vector2Int.down;
-
-        if (coord.x > 0)
-        {
-            if (battleGridTiles[leftCoord.x, leftCoord.y].isWalkable)
-            {
-                walkableNeighbours.AddLast(leftCoord);
-            }
-        }
-
-        if (coord.y > 0)
-        {
-            if (battleGridTiles[bottomCoord.x, bottomCoord.y].isWalkable)
-            {
-                walkableNeighbours.AddLast(bottomCoord);
-            }
-        }
-
-        if (coord.x < gridSizeX - 1)
-        {
-            if (battleGridTiles[rightCoord.x, rightCoord.y].isWalkable)
-            {
-                walkableNeighbours.AddLast(rightCoord);
-            }
-        }
-
-        if (coord.y < gridSizeY - 1)
-        {
-            if (battleGridTiles[topCoord.x, topCoord.y].isWalkable)
-            {
-                walkableNeighbours.AddLast(topCoord);
-            }
-        }
-
-        return walkableNeighbours;
     }
 
     private static void SetAllTilesToDefault()
--- OpenTacticsOnline/Assets/Scripts/GameStates/AbstractGameState.cs
+++ OpenTacticsOnline/Assets/Scripts/GameStates/AbstractGameState.cs
@@ -26,5 +26,6 @@ public abstract class AbstractGameState
     public AbstractGameState(GameState gameState)
     {
         this.gameState = gameState;
+        
     }
 }
